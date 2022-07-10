0169eff266e02f51bdc29d1bff3fe7c63ba002e1
phshy0607 <phshy0607@126.com>
2022-07-11 00:19:48 +0800

    feat: deploy

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   basePath: process.env.NODE_ENV === "development" ? "" : "/blog",
   images: {
-    domains: ["avatars.githubusercontent.com"],
+    loader: "custom",
+    domains: ["githubusercontent.com"],
   },
 };
 
