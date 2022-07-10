8da4cf15233ca195c1fb258ff7dde4952758184a
phshy0607 <phshy0607@126.com>
2022-07-10 22:54:22 +0800

    feat: publicPath

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  basePath: "http://phshy0607.github.io/blog/",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
